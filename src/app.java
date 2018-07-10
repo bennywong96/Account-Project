893e0acb2fe02e03865749489ea2c1a78f8ed2fe
Benny <siunamwongbenny@gmail.com>
2018-07-10 14:15:25 +0100

    Added JSON conversion

--- src/app.java
+++ src/app.java
@@ -1,9 +1,26 @@
+import org.json.JSONObject;
+
+import newApp.Account;
+import newApp.Service;
 
 public class app {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World to the standard out");
+		
+		Account account;
+		Service service;
+	
+		account = new Account("Benny", "Wong", "1");
+		service = new Service();
+		service.addAccount(account);
 
+		
+		
+		JSONObject newJSON = service.convertJSON();
+		System.out.println(newJSON);
+		
+		System.out.println("Hello World to the standard out");
+		
 	}
 
 }
